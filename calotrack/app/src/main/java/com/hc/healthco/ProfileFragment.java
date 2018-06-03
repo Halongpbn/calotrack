d9a0e3aff71f8771bf0c0f84f6941928b2d95020
halongpbn <halongpbn@gmail.com>
2018-06-03 08:39:19 -0700

    Made a lot of small improvements

--- calotrack/app/src/main/java/com/hc/healthco/ProfileFragment.java
+++ calotrack/app/src/main/java/com/hc/healthco/ProfileFragment.java
@@ -3,6 +3,7 @@ package com.hc.healthco;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.Sampler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
@@ -14,6 +15,11 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.hc.healthco.AccountActivity.LoginActivity;
 import com.hc.healthco.R;
 
@@ -21,12 +27,20 @@ public class ProfileFragment extends Fragment {
     private FirebaseAuth firebaseAuth;
     private Button logout;
     private Activity context;
+    private TextView bmr;
+    public DatabaseReference ref;
+    private Button update;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
         firebaseAuth = FirebaseAuth.getInstance();
         logout = (Button) view.findViewById(R.id.logout_button);
+        bmr = (TextView) view.findViewById(R.id.bmr_text);
+        ref = FirebaseDatabase.getInstance().getReference("users");
+        update = (Button) view.findViewById(R.id.updateBMR);
+        String id = ref.push().getKey();
+        bmr.setText(ref.child(id).getValue());
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,6 +50,17 @@ public class ProfileFragment extends Fragment {
                 startActivity(new Intent(context, LoginActivity.class));
             }
         });
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                context = getActivity();
+                startActivity(new Intent(context, SurveyActivity.class));
+            }
+        });
         return view;
+    }
+    private void updateBMR()
+    {
+
     }
 }
