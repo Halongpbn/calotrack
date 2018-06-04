0102f7409da7f54a2ff30ad7625d78440474ba3a
halongpbn <halongpbn@gmail.com>
2018-06-03 17:32:25 -0700

    made date text

--- calotrack/app/src/main/java/com/hc/healthco/AddFragment.java
+++ calotrack/app/src/main/java/com/hc/healthco/AddFragment.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
@@ -13,21 +12,31 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.hc.healthco.AccountActivity.LoginActivity;
 import com.hc.healthco.R;
 
 public class AddFragment extends Fragment {
-    private FloatingActionButton add;
     private CalendarView calendar;
+    private TextView time;
+    private EditText calories;
+    private Button add;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add, container, false);
         calendar = (CalendarView) view.findViewById(R.id.calendar);
+        calories = (EditText) view.findViewById(R.id.calories);
+        add = (Button) view.findViewById(R.id.addCal);
+        time = (TextView) view.findViewById(R.id.date);
+
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = (month + 1) + "/" +dayOfMonth + "/" + year;
+                time.setText(date);
 
             }
         });
