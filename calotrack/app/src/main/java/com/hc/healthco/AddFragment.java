479a09f1a44d1792c3728809033780f0cb5a8d5e
halongpbn <halongpbn@gmail.com>
2018-06-03 18:31:38 -0700

    firebase added for adding calories

--- calotrack/app/src/main/java/com/hc/healthco/AddFragment.java
+++ calotrack/app/src/main/java/com/hc/healthco/AddFragment.java
@@ -15,14 +15,28 @@ import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.hc.healthco.AccountActivity.LoginActivity;
 import com.hc.healthco.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AddFragment extends Fragment {
     private CalendarView calendar;
     private TextView time;
     private EditText calories;
     private Button add;
+    public DatabaseReference ref;
+    private int day;
+    private List<calPoint> points = new ArrayList<>();
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -31,11 +45,19 @@ public class AddFragment extends Fragment {
         calories = (EditText) view.findViewById(R.id.calories);
         add = (Button) view.findViewById(R.id.addCal);
         time = (TextView) view.findViewById(R.id.date);
+        ref = FirebaseDatabase.getInstance().getReference("data");
 
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addPoint();
+            }
+        });
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 String date = (month + 1) + "/" +dayOfMonth + "/" + year;
+                day = dayOfMonth;
                 time.setText(date);
 
             }
@@ -44,6 +66,18 @@ public class AddFragment extends Fragment {
         calendar.setDate(date, true, true);
         return view;
     }
+    public void addPoint()
+    {
+        final double point = Double.parseDouble(calories.getText().toString());
+        String key = ref.push().getKey();
+        calPoint calorie = new calPoint(point, day);
+        DatabaseReference pushedKey = ref.push();
+        points.add(calorie);
+        ref.setValue(points);
+
+
+    }
+
 
 
 
---
+++ calotrack/app/src/main/java/com/hc/healthco/calPoint.java
@@ -0,0 +1,14 @@
+package com.hc.healthco;
+
+public class calPoint {
+    public double numCal;
+    public int day;
+    public calPoint()
+    {
+    }
+    public calPoint(double numCal, int day)
+    {
+        this.numCal = numCal;
+        this.day = day;
+    }
+}
