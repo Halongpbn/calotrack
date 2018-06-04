5edc4831c61015d07a4dcea8b55ae3db81d67a7d
mocha88 <34992468+mocha88@users.noreply.github.com>
2018-06-03 19:50:23 -0700

    added getters and setters

--- calotrack/app/src/main/java/com/hc/healthco/calPoint.java
+++ calotrack/app/src/main/java/com/hc/healthco/calPoint.java
@@ -3,12 +3,29 @@ package com.hc.healthco;
 public class calPoint {
     public double numCal;
     public int day;
+
     public calPoint()
     {
     }
-    public calPoint(double numCal, int day)
-    {
+
+    public calPoint(double numCal, int day) {
         this.numCal = numCal;
         this.day = day;
+    }
+
+    public void setNumCal(double numCal) {
+        this.numCal = numCal;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public double getNumCal() {
+        return numCal;
+    }
+
+    public int getDay() {
+        return day;
     }
 }
